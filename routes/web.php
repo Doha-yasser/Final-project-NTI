4b2c334e0b3bcf8309c0370d99258b3ada1720d2
Doha-yasser <183983657+Doha-yasser@users.noreply.github.com>
2025-09-16 03:12:02 +0300

    add pageNotFound
    Signed-off-by: Doha-yasser <183983657+Doha-yasser@users.noreply.github.com>

--- routes/web.php
+++ routes/web.php
@@ -8,22 +8,36 @@ Route::get('/', function () {
 });
 
 
+Route::get('view', [UserController::class, 'view']);
 
 
-Route::get('/home', [UserController::class ,'home']) -> name('home');
+Route::get('/home', [UserController::class, 'home'])->name('home');
 Route::get('/', [UserController::class, 'index'])->name('index');
 
 
 
 
 Route::prefix('user')->group(function () {
-    Route::get('/' , [UserController::class , 'index']);
-    Route::get('/regist', [UserController::class ,'register' ])->name('regist');
-    Route::post('/regist', [UserController::class , 'store']) -> name('user.store');
-    Route::get('/login', [UserController::class , 'login'])->name('user.login');
-    Route::post('/login', [UserController::class, 'doLogin']) ->name('doLogin');
-    Route::match(['get' , 'post'] , '/check-email', [UserController::class, 'checkMail'])->name('checkMail');
-    Route::match(['get' , 'post'] , '/reset-password', [UserController::class, 'reset'])->name('rest');
+    Route::get('/', [UserController::class, 'index']);
+
+    Route::middleware('login_middleware')->group(function () {
+        Route::get('/regist', [UserController::class, 'register'])->name('regist');
+        Route::post('/regist', [UserController::class, 'store'])->name('user.store');
+        Route::get('/login', [UserController::class, 'login'])->name('user.login');
+        Route::post('/login', [UserController::class, 'doLogin'])->name('doLogin');
+    });
+
+
+    Route::match(['get', 'post'], '/check-email', [UserController::class, 'checkMail'])->name('checkMail');
+    Route::match(['get', 'post'], '/reset-password', [UserController::class, 'reset'])->name('rest');
     Route::get('logout', [UserController::class, 'logout'])->name('logout');
 
+});
+
+
+
+
+// page not found
+Route::fallback(function () {
+    return view('pageNotFound');
 });
